d19b44df0f13e859545729db17a1b0ecc8904396
Yao Zi <ziyao@disroot.org>
2024-08-24 13:27:05 +0000

    Implement shell-like exec

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 CC=cc
 
-"$CC" -o lash.so -shared -fPIC $(pkg-config --cflags lua5.4) lash.c
+"$CC" -o lash.so -shared -fPIC $(pkg-config --cflags lua5.4) lash.c \
+	-Wall -Werror -g -O0 -pedantic -Wextra -std=gnu99
--- lash.c
+++ lash.c
@@ -5,13 +5,17 @@
  *	Copyright (c) 2024 Yao Zi <ziyao at disroot dot org>
  */
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <glob.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include <lua.h>
 #include <lauxlib.h>
-
 
 static int
 raw_glob(lua_State *l)
@@ -33,9 +37,53 @@ raw_glob(lua_State *l)
 	return 1;
 }
 
+static int
+raw_exec(lua_State *l)
+{
+	int n = lua_gettop(l);
+	const char *path = luaL_checkstring(l, 1);
+	const char **argv = malloc(sizeof(char *) * (n + 1));
+	if (!argv)
+		luaL_error(l, "Out of Memory");
+
+	for (int i = 0; i < n; i++) {
+		argv[i] = lua_tostring(l, i + 1);
+		if (!argv[i]) {
+			free(argv);
+			luaL_typeerror(l, i + 1, "string");
+		}
+	}
+	argv[n] = NULL;
+
+	int pid = fork();
+	if (pid > 0) {
+		int status, ret;
+
+		do
+			ret = waitpid(pid, &status, 0);
+		while (ret < 0 && errno == EINTR);
+
+		lua_pushboolean(l, !(!WIFEXITED(status) ||
+				     WEXITSTATUS(status)));
+	} else if (pid == 0) {
+		if (execvp(path, (char * const *)argv)) {
+			fprintf(stderr, "Cannot execute %s: %s\n",
+				        path, strerror(errno));
+			exit(-1);
+		}
+		// never reaches here
+	} else {
+		lua_pushboolean(l, 0);
+	}
+
+	free(argv);
+	return 1;
+}
+
 static const luaL_Reg gLashFunc[] = {
 	{ "glob", raw_glob },
-	{ },
+	{ "exec", raw_exec },
+	{ NULL, NULL },
 };
 
 int
@@ -44,4 +92,3 @@ luaopen_lash_raw(lua_State *l)
 	luaL_newlib(l, gLashFunc);
 	return 1;
 }
-;

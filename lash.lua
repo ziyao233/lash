fbd015053f98947faec9a058ad9f94b43974fa2b
Yao Zi <ziyao@disroot.org>
2024-08-24 13:27:55 +0000

    Support emulating shell-commands in Lua
    With lash.shellcmd() enabled, global variables starting and ending with
    both '_' are recognized as shell command wrappers, which is done by
    mocking the __index metamethod of _G.
    
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- lash.lua
+++ lash.lua
@@ -17,6 +17,50 @@ glob(pattern)
 	end;
 end
 
+local operations = {
+	glob	= glob,
+	exec	= raw.exec,
+};
+
+local function
+getCmdName(s)
+	return s:sub(1, 1) == '_' and s:sub(-1, -1) == '_' and s:sub(2, -2);
+end
+
+local function
+indexCommand(self, k)
+	local cmd = getCmdName(k);
+	return cmd and function(...)
+		return raw.exec(cmd, ...);
+	end or rawget(self, k);
+end
+
+local function
+shellcmd(on)
+	if on then
+		local m = getmetatable(_G) or {};
+		m.__index = indexCommand;
+		setmetatable(_G, m);
+	else
+		local m = getmetatable(_G);
+		if m then
+			m.__index = nil;
+		end
+	end
+end
+
+local function
+exportopt(on)
+	if on then
+		for k, v in pairs(operations) do
+			_G[k] = v;
+		end
+	end
+end
+
 return {
 	glob		= glob,
+	exec		= raw.exec,
+	shellcmd	= shellcmd,
+	exportopt	= exportopt,
        };
